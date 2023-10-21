782b8cc0834b29f1ec5870131bacf530a935231b
gouranga <gm49302@gmail.com>
2023-10-21 15:31:17 +0530

    done: all CRUD issue: put mapping not working properly

--- src/main/java/com/example/db_boot_two/controller/EmployeeController.java
+++ src/main/java/com/example/db_boot_two/controller/EmployeeController.java
@@ -1,9 +1,15 @@
 package com.example.db_boot_two.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.db_boot_two.entity.Employee;
@@ -19,4 +25,23 @@ public class EmployeeController {
         return service.getAll();
     }
 
+    @GetMapping("/all/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable("id") int id) {
+        return service.getEmployeeById(id);
+    }
+
+    @PostMapping("/createEmployee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return service.createEmployee(employee);
+    }
+
+    @DeleteMapping("/deleteEmpl/{id}")
+    public String deleteEmployee(@PathVariable("id") int id) {
+        return service.deleteEmployee(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Employee updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee) {
+        return service.updateEmployee(id, employee);
+    }
 }
--- src/main/java/com/example/db_boot_two/entity/Employee.java
+++ src/main/java/com/example/db_boot_two/entity/Employee.java
@@ -1,16 +1,21 @@
 package com.example.db_boot_two.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Employee {
     @Id
+    @GeneratedValue(generator = "empl_gen", strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "empl_gen", sequenceName = "Employee_gen", initialValue = 0, allocationSize = 0)
     private int id;
     private String name;
     private String address;
     private String branch;
-    private float salary;
+    private int salary;
 
     public int getId() {
         return id;
@@ -44,20 +49,23 @@ public class Employee {
         this.branch = branch;
     }
 
-    public float getSalary() {
+    public int getSalary() {
         return salary;
     }
 
-    public void setSalary(float salary) {
+    public void setSalary(int salary) {
         this.salary = salary;
     }
 
-    public Employee(int id, String name, String address, String branch, float salary) {
+    public Employee(int id, String name, String address, String branch, int salary) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.branch = branch;
         this.salary = salary;
+    }
+
+    public Employee() {
     }
 
     @Override
